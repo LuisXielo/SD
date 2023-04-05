396c6b7d968ca6f74de501973fb633148811b794
LuisXielo <luismi1991.lmmg@gmail.com>
2023-04-05 14:00:23 +0200

    Fin del dia miercoles 5/4/2023

--- app/src/main/java/com/example/sd/MainActivity.java
+++ app/src/main/java/com/example/sd/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.sd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et_nombre, et_contenido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_nombre = (EditText) findViewById(R.id.et_nombre);
+        et_contenido = (EditText) findViewById(R.id.et_contenido);
+    }
+
+    // metodo para boton guardar.
+    public void Guardar(View view){
+        String nombre = et_nombre.getText().toString();
+        String contenido = et_contenido.getText().toString();
+
+        try {
+            File tarjetaSD = Environment.getExternalStorageDirectory();
+            Toast.makeText(this, tarjetaSD.getPath(), Toast.LENGTH_SHORT).show();
+        }catch (IOException r){
+
+        }
     }
 }
