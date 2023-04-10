d24899c2d8423da1df1b5c1336d38504b79b80f0
LuisXielo <luismi1991.lmmg@gmail.com>
2023-04-10 10:16:47 +0200

    añadimos metodo guardar

--- app/src/main/java/com/example/sd/MainActivity.java
+++ app/src/main/java/com/example/sd/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,10 +32,25 @@ public class MainActivity extends AppCompatActivity {
         String contenido = et_contenido.getText().toString();
 
         try {
+            //indicamos donde se encuentra la tarjeta SD
             File tarjetaSD = Environment.getExternalStorageDirectory();
             Toast.makeText(this, tarjetaSD.getPath(), Toast.LENGTH_SHORT).show();
-        }catch (IOException r){
+            // creamos el archivo con el nombre que nos indica el usuario, y el PATH de la tarjeta SD.
+            File rutaArchivo = new File(tarjetaSD.getPath(), nombre);
+            // ahora hay que abrir el archivo.
+            OutputStreamWriter crearArchivo = new OutputStreamWriter(openFileOutput(nombre, Activity.MODE_PRIVATE));
+            // guardamos los datos en el archivo
+            crearArchivo.write(contenido);
+            // limpiamos el buffer
+            crearArchivo.flush();
+            crearArchivo.close();
 
+            Toast.makeText(this, "Se ha guardado correctamente", Toast.LENGTH_SHORT).show();
+            et_nombre.setText("");
+            et_contenido.setText("");
+
+        }catch (IOException e){
+            Toast.makeText(this, "No se puede guardar el archivo", Toast.LENGTH_LONG).show();
         }
     }
 }
